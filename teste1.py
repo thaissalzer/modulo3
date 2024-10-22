6f76fc3cb2b40dc772b57bb707253c90ae238237
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-10-22 12:33:33 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 #imprimindo uma mensagem na tela
 st.write("Sou servidor público!")
+
+
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
+st.markdown("Este é texto")
+st.caption("Esta é a a legenda")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
