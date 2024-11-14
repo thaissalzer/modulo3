275feacb0e1ec106ec128cc4f25e60784a924af0
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 07:07:17 -0300

    Create aula9.py

---
+++ aula9.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+st.write(df)
