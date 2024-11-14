bc23b61ee3ef5cbdcc88a7e9098f7692f665121f
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 07:13:33 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -14,3 +14,8 @@ st.write(df)
 opcao = st.selectbox(
     'Qual servidor você gostaria de selecionar?',
      df['nomeServidor'])
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+dadosFiltrados
