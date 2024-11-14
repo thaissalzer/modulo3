07497bab02ffde54dfee8d0139a27c8a3c71a4f2
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 07:24:51 -0300

    Update quilombolas.py

--- quilombolas.py
+++ quilombolas.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
-df.head()
+st.write(df.head())
