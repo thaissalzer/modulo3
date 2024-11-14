c4a9eeac51ca79214b572317ed2360430ea5e28f
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 07:31:02 -0300

    Update quilombolas.py

--- quilombolas.py
+++ quilombolas.py
@@ -4,3 +4,18 @@ import pandas as pd
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 st.write(df.head())
+
+
+df.fillna(0, inplace=True) 
+df.drop(columns=['Unnamed: 0'], inplace=True)
+list = ['Lat_d', 'Long_d']
+# convertendo para numeros - tenta transformar os valores em números e substitui valores inválidos (como texto que não pode ser convertido) por NaN (Not a Number).
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
