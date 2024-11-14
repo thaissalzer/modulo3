5fcca617f20254b2a6d0de23dc618741d7bb99fe
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 07:34:30 -0300

    Update quilombolas.py

--- quilombolas.py
+++ quilombolas.py
@@ -20,3 +20,5 @@ dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+
+st.write(df.describe())
